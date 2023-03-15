783ab5f0d7a127871b81291248f0666a30beed20
daniloramin <daniloramin00@hotmail.com>
2023-03-15 04:40:14 -0300

    style cards with media queries, add restart button template, change game logic

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -5,40 +5,44 @@ import { GameDifficultyContext } from "../../context/GameDifficultyContext";
 import getCardsBasedOnDifficulty from "../../getCardsBasedOnDifficulty";
 
 function Board(props) {
+  const [showRestart, setShowRestart] = useState(false);
   const [difficulty] = useContext(GameDifficultyContext);
   const [cards, setCards] = useState(getCardsBasedOnDifficulty(difficulty));
-  const pairs = [];
+  const [pairs, setPairs] = useState(0);
   let flipedCards = [];
 
   useEffect(() => {
     setCards(() => getCardsBasedOnDifficulty(difficulty));
   }, [difficulty]);
 
+  useEffect(() => {
+    if (pairs === cards.length) {
+      setShowRestart(() => true);
+      return;
+    }
+  }, [pairs]);
+
   function onChooseCard({ target }) {
-    console.log(pairs);
     const card = target.parentElement;
 
     if (
       (flipedCards.length === 1 && card === flipedCards[0]) ||
-      pairs.includes(card)
+      pairs === cards.length
     )
       return;
 
+    console.log("nao verificou");
+
     if (flipedCards.length === 2) return;
-
-    if (pairs.length === cards.length) return; // win screen
-
-    console.log("passou");
 
     card.className = "card fliped";
 
     if (flipedCards.length === 1) {
       flipedCards.push(card);
-      console.log(flipedCards[0]);
-      console.log(flipedCards[1]);
 
       if (flipedCards[0].dataset.card === flipedCards[1].dataset.card) {
-        pairs.push(flipedCards[0], flipedCards[1]);
+        setPairs((prev) => prev + 2);
+
         flipedCards = [];
         return;
       }
@@ -56,19 +60,39 @@ function Board(props) {
   }
 
   return (
-    <div className="board">
-      {cards.map((card, index) => (
-        <div
-          className="card"
-          onClick={onChooseCard}
-          data-card={card}
-          key={index}
+    <>
+      <div className="board">
+        {cards.map((card, index) => (
+          <div
+            className="card unfliped"
+            onClick={onChooseCard}
+            data-card={card}
+            key={index}
+          >
+            <div className="card-front">🧠</div>
+            <div className="card-back">{card}</div>
+          </div>
+        ))}
+      </div>
+      {showRestart && (
+        <button
+          className="restartButton"
+          onClick={() => {
+            // setCards(() => {
+            //   console.log("primeiro vazio");
+            //   return [];
+            // });
+
+            setCards(() => {
+              console.log("segundo cheio");
+              return getCardsBasedOnDifficulty(difficulty);
+            });
+          }}
         >
-          <div className="card-front">🧠</div>
-          <div className="card-back">{card}</div>
-        </div>
-      ))}
-    </div>
+          X
+        </button>
+      )}
+    </>
   );
 }
 
